cfe06c01ce89c16aa12136ba8a5f9cef1291c35e
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2024-06-07 17:13:22 +0930

    testing geojson import

--- scripts/script.js
+++ scripts/script.js
@@ -4,4 +4,65 @@ document.addEventListener('DOMContentLoaded', function() {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+
+    var geojsonFeature = {
+        "type": "FeatureCollection",
+        "features": [
+          {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+              "coordinates": [
+                [
+                  -0.17628755879957225,
+                  51.51272241113091
+                ],
+                [
+                  -0.1813397079407082,
+                  51.500152514431306
+                ],
+                [
+                  -0.17242834901281867,
+                  51.504405264286845
+                ]
+              ],
+              "type": "LineString"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+              "coordinates": [
+                [
+                  [
+                    -0.16173781654680397,
+                    51.51087386442666
+                  ],
+                  [
+                    -0.15193532304820678,
+                    51.5042202970408
+                  ],
+                  [
+                    -0.14273242864214808,
+                    51.5101352038576
+                  ],
+                  [
+                    -0.15817525035930657,
+                    51.51604716432493
+                  ],
+                  [
+                    -0.16173781654680397,
+                    51.51087386442666
+                  ]
+                ]
+              ],
+              "type": "Polygon"
+            }
+          }
+        ]
+      };
+    
+    L.geoJSON(geojsonFeature).addTo(map);
+
 });
