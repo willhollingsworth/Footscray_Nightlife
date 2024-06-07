65aa123b5a497c2df8427822d4e9e54613cb66e3
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2024-06-07 17:21:05 +0930

    testing different geojson

--- scripts/script.js
+++ scripts/script.js
@@ -6,62 +6,198 @@ document.addEventListener('DOMContentLoaded', function() {
     }).addTo(map);
 
     var geojsonFeature = {
-        "type": "FeatureCollection",
-        "features": [
-          {
-            "type": "Feature",
-            "properties": {},
+    "features": [
+        {
             "geometry": {
-              "coordinates": [
-                [
-                  -0.17628755879957225,
-                  51.51272241113091
+                "coordinates": [
+                    115.860458,
+                    -31.950527
                 ],
-                [
-                  -0.1813397079407082,
-                  51.500152514431306
-                ],
-                [
-                  -0.17242834901281867,
-                  51.504405264286845
-                ]
-              ],
-              "type": "LineString"
-            }
-          },
-          {
-            "type": "Feature",
-            "properties": {},
+                "type": "Point"
+            },
+            "id": 0,
+            "properties": {
+                "marker-color": "#164be9",
+                "marker-size": "large",
+                "marker-symbol": 1,
+                "name": "Perth",
+                "state": "WA",
+                "type": "City"
+            },
+            "type": "Feature"
+        },
+        {
             "geometry": {
-              "coordinates": [
-                [
-                  [
-                    -0.16173781654680397,
-                    51.51087386442666
-                  ],
-                  [
-                    -0.15193532304820678,
-                    51.5042202970408
-                  ],
-                  [
-                    -0.14273242864214808,
-                    51.5101352038576
-                  ],
-                  [
-                    -0.15817525035930657,
-                    51.51604716432493
-                  ],
-                  [
-                    -0.16173781654680397,
-                    51.51087386442666
-                  ]
-                ]
-              ],
-              "type": "Polygon"
-            }
-          }
-        ]
-      };
+                "coordinates": [
+                    115.636093,
+                    -33.327412
+                ],
+                "type": "Point"
+            },
+            "id": 1,
+            "properties": {
+                "marker-color": "#164be9",
+                "marker-size": "medium",
+                "marker-symbol": 2,
+                "name": "Bunbury",
+                "state": "WA",
+                "type": "Town"
+            },
+            "type": "Feature"
+        },
+        {
+            "geometry": {
+                "coordinates": [
+                    117.88473,
+                    -35.02382
+                ],
+                "type": "Point"
+            },
+            "id": 2,
+            "properties": {
+                "marker-color": "#164be9",
+                "marker-size": "medium",
+                "marker-symbol": 3,
+                "name": "Albany",
+                "state": "WA",
+                "type": "Town"
+            },
+            "type": "Feature"
+        },
+        {
+            "geometry": {
+                "coordinates": [
+                    121.891944,
+                    -33.861111
+                ],
+                "type": "Point"
+            },
+            "id": 3,
+            "properties": {
+                "marker-color": "#164be9",
+                "marker-size": "medium",
+                "marker-symbol": 4,
+                "name": "Esperance",
+                "state": "WA",
+                "type": "Town"
+            },
+            "type": "Feature"
+        },
+        {
+            "geometry": {
+                "coordinates": [
+                    121.476013,
+                    -30.74799
+                ],
+                "type": "Point"
+            },
+            "id": 4,
+            "properties": {
+                "marker-color": "#164be9",
+                "marker-size": "medium",
+                "marker-symbol": 5,
+                "name": "Kalgoorlie",
+                "state": "WA",
+                "type": "Town"
+            },
+            "type": "Feature"
+        },
+        {
+            "geometry": {
+                "coordinates": [
+                    [
+                        115.860458,
+                        -31.950527
+                    ],
+                    [
+                        117.88473,
+                        -35.02382
+                    ]
+                ],
+                "type": "LineString"
+            },
+            "id": 5,
+            "properties": {
+                "name": "Perth to Albany",
+                "state": "WA",
+                "stroke": "#ffa200",
+                "stroke-opacity": 1,
+                "stroke-width": 3,
+                "type": "Link"
+            },
+            "type": "Feature"
+        },
+        {
+            "geometry": {
+                "coordinates": [
+                    [
+                        [
+                            119.63876329950443,
+                            -32.03845280379327
+                        ],
+                        [
+                            120.04863847416522,
+                            -32.03816405395313
+                        ],
+                        [
+                            120.04685552432949,
+                            -31.483145363576604
+                        ],
+                        [
+                            119.67900576408238,
+                            -31.483295898284062
+                        ],
+                        [
+                            119.8115377286232,
+                            -31.691546441283215
+                        ],
+                        [
+                            119.63887265060669,
+                            -31.771663437684396
+                        ],
+                        [
+                            119.63880885005983,
+                            -31.93077197638293
+                        ],
+                        [
+                            119.65615009132767,
+                            -31.930773454367554
+                        ],
+                        [
+                            119.65612610930879,
+                            -31.94494116120559
+                        ],
+                        [
+                            119.63877961686802,
+                            -31.944941948631836
+                        ],
+                        [
+                            119.63876329950443,
+                            -32.03845280379327
+                        ]
+                    ]
+                ],
+                "type": "Polygon"
+            },
+            "id": 6,
+            "properties": {
+                "fill": "#555555",
+                "fill-opacity": 0,
+                "marker-color": "#000000",
+                "marker-size": "",
+                "marker-symbol": "",
+                "name": "Jilbadji Nature Reserve",
+                "state": "WA",
+                "stroke": "#fe0bf6",
+                "stroke-opacity": 1,
+                "stroke-width": 2,
+                "type": "Boundary"
+            },
+            "type": "Feature"
+        }
+    ],
+    "type": "FeatureCollection"
+};
     
     L.geoJSON(geojsonFeature).addTo(map);
 
